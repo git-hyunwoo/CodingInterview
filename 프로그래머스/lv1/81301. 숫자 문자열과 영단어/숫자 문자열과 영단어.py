6677dc88a69e147cf58a70ad54c3a769c1eb4a56
Hungry Developer <49427835+Jack-Snider@users.noreply.github.com>
2023-07-23 21:53:09 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,18 @@
+def solution(n):
+    answer = n
+
+    answer = answer.replace('zero','0')
+    answer = answer.replace('one','1')
+    answer = answer.replace('two','2')
+    answer = answer.replace('three','3')
+    answer = answer.replace('four','4')
+    answer = answer.replace('five','5')
+    answer = answer.replace('six','6')
+    answer = answer.replace('seven','7')
+    answer = answer.replace('eight','8')
+    answer = answer.replace('nine','9')
+    
+
+    return int(answer)
+
+print(solution('2three45sixseven'))
