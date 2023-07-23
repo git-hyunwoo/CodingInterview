d499940374110ab1d2b97ee6e400c0d57b9ab66b
Hungry Developer <49427835+Jack-Snider@users.noreply.github.com>
2023-07-23 22:34:31 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,18 +1,23 @@
 def solution(n):
-    answer = n
 
-    answer = answer.replace('zero','0')
-    answer = answer.replace('one','1')
-    answer = answer.replace('two','2')
-    answer = answer.replace('three','3')
-    answer = answer.replace('four','4')
-    answer = answer.replace('five','5')
-    answer = answer.replace('six','6')
-    answer = answer.replace('seven','7')
-    answer = answer.replace('eight','8')
-    answer = answer.replace('nine','9')
+    dictionary = {
+        '0':'zero',
+        '1':'one',
+        '2':'two',
+        '3':'three',
+        '4':'four',
+        '5':'five',
+        '6':'six',
+        '7':'seven',
+        '8':'eight',
+        '9':'nine',
+    }
     
+    for key,value in dictionary.items():
+        if value in n:
+            n = n.replace(value,key)
+    
+    answer = n
 
     return int(answer)
 
-print(solution('2three45sixseven'))
