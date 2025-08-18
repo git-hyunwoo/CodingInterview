a3073041525f17cea32d5f57adf4a8069294327a
hyunwoo <choiman2021@gmail.com>
2025-08-18 23:20:37 +0900

    58. Length of Last Word.py

--- LeetCode/Easy/58. Length of Last Word.py
+++ LeetCode/Easy/58. Length of Last Word.py
@@ -4,16 +4,19 @@ class Solution:
         tmp = ''
         for v in s:
             if v != '' and v != ' ':
-                tmp += v    
+                tmp += v   
             else:
+                print(f' => {tmp}')
                 if tmp != '':
                     lst.append(tmp)
                 tmp = ''
 
+        if tmp != '':
+            lst.append(tmp)
         # To get rid of '' in lst
         #lst = [v for v in lst if v != '']
-        
+
         return len(lst[-1])
     
 a = Solution()
-print(a.lengthOfLastWord('   fly me   to   the moon  '))
+print(a.lengthOfLastWord('luffy is still joyboy'))
